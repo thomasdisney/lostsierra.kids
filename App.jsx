c64802262f1d87113de15e3805270e0a208971a3
thomasdisney <149925753+thomasdisney@users.noreply.github.com>
2025-06-23 13:01:58 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,23 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
-import LoginPage from "./LoginPage";
 import Dashboard from "./Dashboard";
-import ProtectedRoute from "./ProtectedRoute";
+import LoginPage from "./LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </BrowserRouter>
   );
